3fe98467fd5a65ce55d3b36cd962cf80b5343eb2
Carol <cg_199505@hotmail.com>
2020-02-29 14:30:35 -0500

    Singleton
    Singleton design pattern study

---
+++ singletonMethod/singletonMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singletonMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Random random1 = new Random();
+                int v1 = random1.Next(0, 10);
+                Console.WriteLine("Random 1: " + v1);
+            }
+            Console.WriteLine("Random Singleton: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Random random1 = RandomGenerator.GetInstance();
+                int v1 = random1.Next(0, 10);
+                Console.WriteLine("Random 1: " +v1);
+            }
+        }
+    }
+
+}
---
+++ singletonMethod/singletonMethod/RandomGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singletonMethod
+{
+    class RandomGenerator
+    {
+        private static Random myInstance;
+        public RandomGenerator() { }
+        public static Random GetInstance()
+        {
+            if (myInstance == null)
+            {
+                myInstance = new Random();
+            }
+            return myInstance;
+        }
+    }
+
+}
+
